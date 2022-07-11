4eee4b8d20da3cedbe4547a8fb82748d5ec4d675
Matthias Lange <matthias.lange@utah.edu>
2022-07-11 12:38:12 -0600

    More efficcient implementation of get sub surface

--- getSubSurface.m
+++ getSubSurface.m
@@ -1,4 +1,4 @@
-function [nodes, faces,nodesKeeped,deleteFaces] = getSubSurface(nodes, faces, data,subId)
+function [newNodes, newFaces,nodesKeeped,deleteFaces] = getSubSurface(nodes, faces, data,subId)
 %GETSUBSURFACE Extract the part which has subId
 %   From a surface mesh, all nodes that are subId will be extraced 
 %   and remaining surface be 
@@ -10,63 +10,25 @@ if (size(faces,1)~= length(faces))
     faces=faces';
 end
 
-toKeep =(data~=subId);
-idToKeep=1:length(data);
-idToKeep(~toKeep)=[];%This is a ordered list
+%Which nodes to keep
+toKeep =(data==subId);
+%Which faces to keep
+facesToKeep = sum(toKeep(faces),2)>1  ;
+allUsedNodes = unique(faces(facesToKeep,:));
+newNodes = nodes (allUsedNodes,:);
+%Map old id to new
+oldNodesToNew=zeros(size(nodes,1),1);
+for i = 1:size(allUsedNodes,1)
+	oldNodesToNew(allUsedNodes(i) )=i ;
+end 
+%Get the faces and convert there indices
+newFaces = faces(facesToKeep,:);
+newFaces=oldNodesToNew(newFaces);
 
-  
-%for i=1:length(idToKeep)
-deleteFaces = zeros( size(faces,1),1);
-for i=1:size(faces,1)      
-    for j=1:size(faces,2)
-        
-        if sum(faces(i,j)== idToKeep)>0
-            
-            deleteFaces(i)=1;
-           break;
-        else
-            ind=find((idToKeep-faces(i,j))>0,1,'first')-1;
-            if ind>0
-          %      faces(i,j)=faces(i,j)-ind;
-            end
-        end
-        
-    end
-end
-faces(logical(deleteFaces),:)=[];
+nodesKeeped=false(size(nodes,1),1);
+nodesKeeped(allUsedNodes)=true;
 
-% check for unrefered points
-s=size(nodes);
-tic
-tests=1:s(1);
-tests(toKeep)=[];
-disp(['Remove before ' num2str(size(toKeep))]);
-for i =tests
-    if isempty(find(faces==i,1))
-        %found non used node 
-        toKeep(i)=1;
-        
-    end 
-end
-disp(['Remove after ' num2str(size(toKeep))]);
-toc
-% remove the points 
-nodes=nodes(~toKeep,:);
-[sf,sfi]=sort(faces(:));
-nodesGone=sort(find(toKeep));
-last=1;
-sfnew=sf;
-if ~isempty(nodesGone)
-    for i = 1:size(nodesGone,1)
-        last=find(sf>=nodesGone(i),1);
-        sfnew(last:end)=sfnew(last:end)-1;
-    end     
+deleteFaces= 1: size(faces,1);
+deleteFaces(facesToKeep)=[];
 
-    sf(sfi)=sfnew;
-    faces=reshape(sf,size(faces));
-end
-    %faces(faces>idToKeep(i))=faces(faces>idToKeep(i))-1;    
-    %nanvertices(i+1:end)=nanvertices(i+1:end)-1;
-nodesKeeped=~toKeep;
-end
-
+end 
